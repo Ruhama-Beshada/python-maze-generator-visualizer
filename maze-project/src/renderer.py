a624f90490e0dcaa94ea282a4352346eca0a2f76
Ruhama Beshada <ruhamabeshada@gmail.com>
2026-05-14 16:53:04 +0300

    Implemented maze grid rendering

---
+++ maze-project/src/renderer.py
@@ -0,0 +1,8 @@
+import pygame
+from constants import *
+
+# WALL DATA
+northWall = [[1 for _ in range(COLS)] for _ in range(ROWS + 1)]
+eastWall = [[1 for _ in range(COLS + 1)] for _ in range(ROWS)]
+
+
